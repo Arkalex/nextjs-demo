f74087f188adfd1be2ab15160eb960c3247bb4db
Alex Ayllon <alex.ayllon@lecreuset.com>
2020-12-30 17:36:49 +0100

    testing static props

--- pages/info/index.js
+++ pages/info/index.js
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 import styles from '../../styles/Home.module.css'
 
-export default function Info(params) {
+export default function Info({ username }) {
     return (
         <>
             <main className={styles.main}>
                 <h1 className={styles.title}>
                     This is just a test for routes
                 </h1>
+                <h2>Developed by {username}</h2>
 
                 <nav className={styles.nav}>
                     <Link href="/">
@@ -19,4 +20,12 @@ export default function Info(params) {
             </main>
         </>
     )
+}
+
+export const getStaticProps = async () => {
+    return {
+        props: {
+            username: 'Alex'
+        }
+    }
 }
