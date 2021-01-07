c361a8ff6bbe1f9da3c9190d0e10f0c8f2a5e4d7
Alex Ayllon <alex.ayllon@lecreuset.com>
2021-01-07 17:12:03 +0100

    Refactored some code. Added styled components. Created home

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,48 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-import {useRouter} from 'next/router'
 import AppLayout from '../components/AppLayout'
+
+import { colors } from '../styles/theme'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Next.js Demo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <AppLayout>
-        <h1 className={styles.title}>
-          <a href="https://nextjs.org">Next.js! demo</a>
-        </h1>
-
-        <nav className={styles.nav}>
-          <Link href="/timeline">
-            <a>
-              Testing routes
-            </a>
-          </Link>
-        </nav>
+        <section>
+          <img src="/twitter-logo.png" />
+          <h1>Twitter</h1>
+          <h2>Developed with Next.js!</h2>
+        </section>
       </AppLayout>
 
+      <style jsx>{`
+        img {
+          width: 120px;
+        }
+
+        section{
+          display: grid;
+          height: 100%;
+          place-items: center;
+          place-content: center;
+        }
+
+        h1 {
+          color: ${colors.primary};
+          font-weight: 800
+          margin-bottom: 8px;
+        }
+
+        h2 {
+          color: ${colors.secondary};
+          font-size: 21px;
+          margin: 0;
+        }
+      `}</style>
     </div>
   )
 }
